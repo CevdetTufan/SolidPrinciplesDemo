c96870ec0f6ea419fe270517dfa496c55e31ddfd
Cevdet Tufan <cevdet.tufan@hotmail.com>
2024-05-20 20:45:15 +0300

    Project name changed.

--- SolidPrinciplesDemoLcp/Program.cs
+++ SolidPrinciplesDemoLcp/Program.cs
@@ -1,3 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//https://www.gokhan-gokalp.com/liskov-substitution-principle-lsp-liskovun-yerine-gecme-prensibi/
+﻿
