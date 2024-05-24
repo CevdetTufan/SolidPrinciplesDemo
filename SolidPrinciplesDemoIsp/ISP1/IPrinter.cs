835e897fdfb05992ebccfdc85ff805618ca28a9c
Cevdet Tufan <cevdet.tufan@hotmail.com>
2024-05-24 21:40:18 +0300

    #1 - Interface Segration Principle

---
+++ SolidPrinciplesDemoIsp/ISP1/IPrinter.cs
@@ -0,0 +1,29 @@
+﻿namespace SolidPrinciplesDemoIsp.ISP1
+{
+	public interface IPrinter
+	{
+		void Print(string document);
+		void Scan(string document);
+		void Fax(string document);
+	}
+
+
+	//The class was forced to use methods it could not use.
+	public class SimplePrinter : IPrinter
+	{
+		public void Print(string document)
+		{
+			// Print the document
+		}
+
+		public void Scan(string document)
+		{
+			//do not use
+		}
+
+		public void Fax(string document)
+		{
+			//do not use
+		}
+	}
+}
---
+++ SolidPrinciplesDemoIsp/ISP1/IPrinterIsp.cs
@@ -0,0 +1,44 @@
+﻿namespace SolidPrinciplesDemoIsp.ISP1
+{
+	public interface IPrinterIsp
+	{
+		void Print(string document);
+	}
+
+	public interface IScannerIsp
+	{
+		void Scan(string document);
+	}
+
+	public interface IFaxIsp
+	{
+		void Fax(string document);
+	}
+
+	//Class implements only the methods it needs
+	public class SimplePrinterIsp : IPrinterIsp
+	{
+		public void Print(string document)
+		{
+			Console.WriteLine($"Print : {document}");
+		}
+	}
+
+	public class MultifunctionPrinter : IPrinterIsp, IScannerIsp, IFaxIsp
+	{
+		public void Fax(string document)
+		{
+            Console.WriteLine($"Fax : {document}");
+        }
+
+		public void Print(string document)
+		{
+			Console.WriteLine($"Print : {document}");
+		}
+
+		public void Scan(string document)
+		{
+			Console.WriteLine($"Scan : {document}");
+		}
+	}
+}
